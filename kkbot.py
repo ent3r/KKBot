892aab4c2f8143ab706af74b86db7348b4707bc2
Nikolai <32072697+ent3r@users.noreply.github.com>
2020-02-28 16:24:52 +0100

    Remove last command dict as not needed

--- kkbot.py
+++ kkbot.py
@@ -29,7 +29,6 @@ PREFIX = "!"
 # endregion
 
 BOT = commands.Bot(command_prefix=PREFIX, description=DESCRIPTION)
-LAST_COMMAND = {"exit code": None, "command": None, "params": None}
 
 
 async def handle_error(ctx, error, readable_error=None):
@@ -83,8 +82,6 @@ async def math(ctx):
 @math.command()
 async def add(ctx, *numbers):
     """Adds two numbers together."""
-    LAST_COMMAND["command"] = "add"
-    LAST_COMMAND["args"] = numbers
     numbers_int = []
     try:
         for number in numbers:
@@ -92,7 +89,6 @@ async def add(ctx, *numbers):
     except ValueError:
         await ctx.send("Error: Cannot convert letters to int")
         print("Add command failed")
-        LAST_COMMAND["exit code"] = 1
         return
     await ctx.send(sum(numbers_int))
     print("Add command succeed")
@@ -101,17 +97,13 @@ async def add(ctx, *numbers):
 @math.command()
 async def sqrt(ctx, number):
     """Finds the square root of any number"""
-    LAST_COMMAND["command"] = "square"
-    LAST_COMMAND["params"] = None
     try:
         number = int(number)
     except ValueError:
         await ctx.send("Error: Cannot convert letter to int")
-        LAST_COMMAND["exit code"] = 1
         return
     square = mathfunc.sqrt(number)
     await ctx.send(f"The square root of {number} is {square}\n┏┓\n┗┛")
-    LAST_COMMAND["exit code"] = 0
 
 # endregion
 
@@ -137,12 +129,6 @@ async def status(ctx):
 
     content.add_field(name="Last downtime", value=datetime.datetime.fromtimestamp(
         data["datetime"]), inline=True)
-    content.add_field(name="Last Command",
-                      value=LAST_COMMAND["command"], inline=False)
-    content.add_field(name="Exit code", value=str(
-        LAST_COMMAND["exit code"]), inline=True)
-    content.add_field(name="Parameters",
-                      value=LAST_COMMAND["params"], inline=True)
     await ctx.send(embed=content)
 
 
@@ -155,93 +141,63 @@ async def ping(ctx):
 @BOT.command()
 async def randomcommand(ctx):
     """Just try it."""
-    LAST_COMMAND["command"] = "randomcommand"
-    LAST_COMMAND["params"] = None
-    LAST_COMMAND["exit code"] = 0
     await ctx.send("Why are you typing bullshit?")
 
 
 @BOT.command()
 async def groot(ctx):
     """I am Groot"""
-    LAST_COMMAND["command"] = "groot"
-    LAST_COMMAND["params"] = None
-    LAST_COMMAND["exit code"] = 0
     await ctx.send("I am Groot")
 
 
 @BOT.command()
 async def botname(ctx):
     """Tells you the BOT's name."""
-    LAST_COMMAND["command"] = "botname"
-    LAST_COMMAND["params"] = None
-    LAST_COMMAND["exit code"] = 0
     await ctx.send(BOT.user.name)
 
 
 @BOT.command()
 async def roll(ctx, dice: str):
     """Rolls a dice in NdN format."""
-    LAST_COMMAND["command"] = "randomcommand"
-    LAST_COMMAND["params"] = dice
     try:
         rolls, limit = map(int, dice.split('d'))
     except ValueError:
         await ctx.send('Format has to be in NdN!')
-        LAST_COMMAND["exit code"] = 1
         return
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
-    LAST_COMMAND["exit code"] = 0
 
 
 @BOT.command(description='For when you wanna settle the score some other way')
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
-    LAST_COMMAND["command"] = "choose"
-    LAST_COMMAND["params"] = None
-    LAST_COMMAND["exit code"] = 1
     await ctx.send(random.choice(choices))
-    LAST_COMMAND["exit code"] = 0
 
 
 @BOT.command()
 async def repeat(ctx, times: int, *content):
     """Repeats a message multiple times."""
-    LAST_COMMAND["command"] = "repeat"
-    LAST_COMMAND["params"] = None
-    LAST_COMMAND["exit code"] = 1
     for _ in range(times):
         await ctx.send(content)
         time.sleep(0.2)
-    LAST_COMMAND["exit code"] = 0
 
 
 @BOT.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
-    LAST_COMMAND["command"] = "joined"
-    LAST_COMMAND["params"] = None
-    LAST_COMMAND["exit code"] = 1
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-    LAST_COMMAND["exit code"] = 0
 
 
 @BOT.group()
 async def cool(ctx):
     """Says if a user is cool."""
-    LAST_COMMAND["command"] = "cool"
-    LAST_COMMAND["params"] = None
-    LAST_COMMAND["exit code"] = 1
     if ctx.invoked_subcommand is None:
         if random.randint(0, 1) == 0:
             await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-            LAST_COMMAND["exit code"] = 0
             return
         else:
             await ctx.send('Yes, {0.subcommand_passed} is cool :)'.format(ctx))
-            LAST_COMMAND["exit code"] = 0
             return
 
 TOKEN = os.getenv("KKBOTTOKEN")
